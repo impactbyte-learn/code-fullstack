a7d95e46337ecf0a4b53a743bf20bc6752fea1a8
M Haidar Hanif <git@mhaidarhanif.com>
2017-10-30 14:34:13 +0700

    Display data via root route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express")
+const router = express.Router()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const DATA = require("./data.json")
 
-module.exports = router;
+router.get("/", (req, res, next) => {
+  res.send(DATA)
+})
+
+module.exports = router
