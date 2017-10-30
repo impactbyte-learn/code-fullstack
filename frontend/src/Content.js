481bffca5d9248073f9251a9de1a99901f44d8c1
M Haidar Hanif <git@mhaidarhanif.com>
2017-10-30 16:35:43 +0700

    Display each users

--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -2,6 +2,19 @@ import React, { Component } from "react"
 
 export default class Content extends Component {
   render() {
-    return <div>{JSON.stringify(this.props.data)}</div>
+    return (
+      <div>
+        {this.props.data.map(user => {
+          return (
+            <div key={user.id}>
+              <h3>{user.name}</h3>
+              <h3>{user.username}</h3>
+              <h3>{user.email}</h3>
+              <br />
+            </div>
+          )
+        })}
+      </div>
+    )
   }
 }
