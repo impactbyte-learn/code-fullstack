92fce303b2ab7caa7d85e9b3f63496fb91efab7f
M Haidar Hanif <git@mhaidarhanif.com>
2017-10-30 15:31:50 +0700

    Fetch and display data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 
-const API_URL = "https://backend-xnmfwjkobs.now.sh"
+const API_URL = "http://localhost:8000"
 
 class App extends Component {
   constructor(props) {
@@ -10,22 +10,18 @@ class App extends Component {
     }
   }
 
-  async getInitialState() {
+  async componentWillMount() {
     const RESPONSE = await fetch(API_URL)
     const DATA = await RESPONSE.json()
-    console.log(API_URL)
-    console.log(DATA)
-    this.setState(state => {
-      return {
-        data: DATA
-      }
+    this.setState({
+      data: DATA
     })
   }
 
   render() {
     return (
       <div id="container">
-        <p>{API_URL}</p>
+        <p>{JSON.stringify(this.state.data)}</p>
       </div>
     )
   }
