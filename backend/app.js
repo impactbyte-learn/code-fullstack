8620845b762ac81adb64ec5b03566c9f7adbc71a
M Haidar Hanif <git@mhaidarhanif.com>
2017-10-30 15:16:18 +0700

    Install cors and change port

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,16 @@
-var express = require("express")
-var path = require("path")
-var favicon = require("serve-favicon")
-var logger = require("morgan")
-var cookieParser = require("cookie-parser")
-var bodyParser = require("body-parser")
+const express = require("express")
+const path = require("path")
+const favicon = require("serve-favicon")
+const logger = require("morgan")
+const cookieParser = require("cookie-parser")
+const bodyParser = require("body-parser")
+
+const cors = require("cors")
 
 // -----------------------------------------------------------------------------
 
-var index = require("./routes/index")
-var users = require("./routes/users")
+const index = require("./routes/index")
+const users = require("./routes/users")
 
 // -----------------------------------------------------------------------------
 
@@ -32,6 +34,7 @@ app.use(express.static(path.join(__dirname, "public")))
 
 // -----------------------------------------------------------------------------
 
+app.use(cors())
 app.use("/", index)
 app.use("/users", users)
 
